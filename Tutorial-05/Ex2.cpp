04370c0ed625964aed45054f62f423ee6a8b5ba2
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-03-27 12:59:41 +0000

    coding standards

--- Tutorial-05/Ex2.cpp
+++ Tutorial-05/Ex2.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()   //begin main
 {
-  float num1;
+  float num1;    //defining variables
   float num2;
 
-  cout << "Enter Number 1: " <<endl;
+  cout << "Enter Number 1: " <<endl;  //user input
   cin >> num1;
 
   cout << "Enter Number 2: " <<endl;
   cin >> num2;
   
-  float *fPtr1;  
+  float *fPtr1;    //pointer declaration
   float *fPtr2;
   
-  fPtr1 = &num1;  
+  fPtr1 = &num1;  //assigning var
   cout << *fPtr1;
 
   fPtr2 = &num2;  
@@ -23,9 +23,9 @@ int main()
   
   *fPtr1 ;  
   
-  cout << number2 <<endl;  
+  cout << number2 <<endl;    //display output
   cout << number1 <<endl;  
  
   
-  return num1 + num2;
-}
+  return num1 + num2;    //return addition
+}//end main
