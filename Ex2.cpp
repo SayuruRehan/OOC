ae645fa3f02b0b226b706e614e8325dcc977843b
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-03-30 16:32:33 +0000

    coding standards

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,24 +1,24 @@
-#include <conio.h>
+#include <conio.h>    //header files
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-void findMarks(int t_marks1, int t_marks2, float& t_CA1, float& t_CA2);
+void findMarks(int t_marks1, int t_marks2, float& t_CA1, float& t_CA2);  //function declaration
 
-struct marks
+struct marks    //create structure marks
 {
 	int t_marks1, t_marks2;
 	int student;
 	float t_CA1, t_CA2;
 };
 
-int main() 
+int main() //begin main
 {
 	marks details[5];
 	
-	for (int i = 0; i < 5; i++) 
+	for (int i = 0; i < 5; i++) //begin for loop
 	{
-		cout << "Enter mark 1 for student " << i + 1 << ": ";
+		cout << "Enter mark 1 for student " << i + 1 << ": ";    //user input
 		cin >> details[i].t_marks1;
 		
 		cout << "Enter mark 2 for student " << i + 1 << ": ";
@@ -29,16 +29,16 @@ int main()
 		details[i].student = i + 1;
 		
 		findMarks(details[i].t_marks1, details[i].t_marks2, details[i].t_CA1, details[i].t_CA2);
-	}
+	}  //end for loop
 	
 	cout << endl;
 	
-	cout << setw(10) << "student" << setw(10) << "Mark 1" << setw(10) << "Mark 2" << setw(10) << "CA 1" << setw(10) << "CA 2" << endl;
+	cout << setw(10) << "student" << setw(10) << "Mark 1" << setw(10) << "Mark 2" << setw(10) << "CA 1" << setw(10) << "CA 2" << endl;    //output
 	
-	for (int i = 0; i < 5; i++) 
+	for (int i = 0; i < 5; i++) //begin for loop
 	{
 		cout << setw(10) << details[i].student << setw(10) << details[i].t_marks1 << setw(10) << details[i].t_marks2 << setw(10) << setiosflags(ios::fixed) << setprecision(2) << details[i].t_CA1 << setw(10) << details[i].t_CA2 << endl;
-	}
+	}    //end for loop
 	
 	(void)getchar();
 	//return 0;
