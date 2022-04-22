5074d52d51da007e179562cf5f3fc5d2402a3fa6
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-04-22 02:38:49 +0000

    lab7-distance.cpp

--- Lab-07/Distance.cpp
+++ Lab-07/Distance.cpp
@@ -1,1 +1,45 @@
 #include "Distance.h"
+#include "iostream"
+using namespace std;
+
+Distance::Distance() {
+	feet = 0;
+	inches = 0.0;
+}
+
+
+Distance::Distance(int ft, float in) {
+	feet = ft;
+	inches = in;
+
+}
+
+void Distance::inputDistance() {
+	cout << "Enter No of feet : ";
+	cin >> feet;
+	cout << "Enter No of inches : ";
+	cin >> inches;
+
+}
+
+
+void Distance::printDistance() {
+	cout << "feet = " << feet << " and ";
+	cout << "inches = " << inches << endl;
+
+}
+
+Distance::~Distance() {
+	cout << "Distance deleted" << endl;
+}
+
+void Distance::addDistance(Distance d2, Distance d3)
+{
+	inches = d2.inches + d3.inches;
+	if (inches >= 12.0)
+	{
+		inches = inches - 12.0;
+		feet++;
+	}
+	feet = feet + (d2.feet + d3.feet);
+}
